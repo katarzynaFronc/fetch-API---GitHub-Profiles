7e84e487ddb3abb9ca465bb45b033ed801c150f3
Katarzyna Fronc <katarzynaanna.fronc@gmail.com>
2023-01-11 19:21:57 +0100

    user repos list function was added

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
 const baseUrl = "https://api.github.com";
-
-//----------------------------------------------------------------
-
-async function getUsers() {
-  const usersResponse = await fetch(`${baseUrl}/users`);
-  const users = await usersResponse.json();
-  console.log(users);
-  return users;
-}
-
-getUsers();
-
-//----------------------------------------------------------------
-
-// const fetchUsers = fetch(`${baseUrl}`)
-//   .then((result) => result.json())
-//   .then((user) => {
-//     console.log(user);
-//   });
-
-//----------------------------------------------------------------
+const defaultUrl = "https://api.github.com/users/dominikmlynarczyk";
 
 const userAvatar = document.getElementById("avatar");
 const userName = document.getElementById("name");
@@ -27,22 +7,45 @@ const userDescription = document.getElementById("description");
 const userFollowers = document.getElementById("followers");
 const userFollowing = document.getElementById("following");
 const userRepositories = document.getElementById("repositories");
-
-const defaultUser = fetch(`${baseUrl}/users/dominikmlynarczyk`)
-  .then((result) => result.json())
-  .then((user) => {
-    userAvatar.innerHTML = `<img src="${user.avatar_url}"/>`;
-    userName.innerHTML = `${user.name}`;
-    userDescription.innerHTML = `${user.bio}`;
-    userFollowers.innerHTML = `${user.followers} Followers`;
-    userFollowing.innerHTML = `${user.following} Following`;
-    userRepositories.innerHTML = `${user.public_repos} repositories`;
-  });
-
+const userRepos = document.getElementById("repos");
 const userSearch = document.querySelector(".input__field");
-userSearch.addEventListener("change", function () {
-  let userSearch = document.querySelector(".input__field").value;
-  fetch(`${baseUrl}/users/${userSearch}`)
+
+//-----------------------default user's repos----------------------
+
+async function defaultUserRepos() {
+  const addRepos = document.getElementById("repos");
+  await fetch(`${defaultUrl}/repos`)
+    .then((result) => result.json())
+    .then(function (data) {
+      data.forEach((repo) => {
+        const newLink = document.createElement("a");
+        newLink.innerHTML = `<a href="${repo.svn_url}" target ="_blank" >${repo.name}</a>`;
+        addRepos.appendChild(newLink);
+      });
+    });
+}
+
+//-----------------------search user's repos----------------------
+function searchUserRepos() {
+  userSearch.addEventListener("change", function () {
+    let userSearch = document.querySelector(".input__field").value;
+    const addRepos = document.getElementById("repos");
+    fetch(`${baseUrl}/users/${userSearch}/repos`)
+      .then((result) => result.json())
+      .then(function (data) {
+        data.forEach((repo) => {
+          const newLink = document.createElement("a");
+          newLink.innerHTML = `<a href="${repo.svn_url}" target ="_blank" >${repo.name}</a>`;
+          addRepos.appendChild(newLink);
+        });
+      });
+  });
+}
+
+//--------------------------default user--------------------------
+
+async function defaultUser() {
+  await fetch(`${defaultUrl}`)
     .then((result) => result.json())
     .then((user) => {
       userAvatar.innerHTML = `<img src="${user.avatar_url}"/>`;
@@ -52,4 +55,27 @@ userSearch.addEventListener("change", function () {
       userFollowing.innerHTML = `${user.following} Following`;
       userRepositories.innerHTML = `${user.public_repos} repositories`;
     });
-});
+  defaultUserRepos();
+}
+defaultUser();
+
+//--------------------------search user--------------------------
+async function searchUser() {
+  await userSearch.addEventListener("change", function () {
+    const userSearch = document.querySelector(".input__field").value;
+    fetch(`${baseUrl}/users/${userSearch}`)
+      .then((result) => result.json())
+      .then((user) => {
+        userAvatar.innerHTML = `<img src="${user.avatar_url}"/>`;
+        userName.innerHTML = `${user.name}`;
+        userDescription.innerHTML = `${user.bio}`;
+        userFollowers.innerHTML = `${user.followers} Followers`;
+        userFollowing.innerHTML = `${user.following} Following`;
+        userRepositories.innerHTML = `${user.public_repos} repositories`;
+      });
+  });
+  searchUserRepos();
+}
+searchUser();
+
+//----------------------------------------------------------------
